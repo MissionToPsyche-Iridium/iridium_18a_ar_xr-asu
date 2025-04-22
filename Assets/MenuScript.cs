c158de2d4b3f30eb6fb549c4ad791fe9a971a59e
Allyson Huang <91769429+ahuangasu@users.noreply.github.com>
2025-04-21 20:34:58 -0700

    Add Start Screen
    add starting screen and menu to scene

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject credits;
+    public GameObject menuButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("start");
+        mainMenu.SetActive(true);
+        credits.SetActive(false);
+        menuButton.SetActive(false);
     }
 
     public void OpenPsycheScene()
     {
+        Debug.Log("open called");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Scene scene = SceneManager.GetSceneAt(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log(scene.name);
     }
-    public void ExitGame()
+
+    public void openMenu()
+    {
+        Debug.Log("Opening menu");
+        mainMenu.SetActive(true);
+        credits.SetActive(false);
+        menuButton.SetActive(false);
+    }
+
+    public void openCredits()
+    {
+        Debug.Log("Open credits");
+        credits.SetActive(true);
+        mainMenu.SetActive(false);
+        menuButton.SetActive(false);
+    }
+
+    public void showMenuButton()
+    {
+        menuButton.SetActive(true);
+    }
+    public void QuitGame()
     {
         Debug.Log("Exit");
         Application.Quit();
