3a512c2c26fd1c14239565b69ca4eb7518e8eed7
Pajeremy <111713502+Pajeremy@users.noreply.github.com>
2025-04-28 13:29:05 -0700

    Update SpacecraftPart.cs
    Modified Script to work better

--- Assets/Scenes/PsycheScene/Scripts/SpacecraftPart.cs
+++ Assets/Scenes/PsycheScene/Scripts/SpacecraftPart.cs
@@ -1,43 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 public class SpacecraftPart : MonoBehaviour
 {
-    public string name;
-    public GameObject part;
     public GameObject panel;
-
-    private void Start()
-    {
-        hidePanel();
-    }
-
-    void OnGUI()
-    {
-        if (Selection.activeGameObject == part)
-        {
-            showPanel();
-        }
-        else
-        {
-            hidePanel();
-        }
-    }
-
-    public GameObject getPart()
-    {
-        return this.part;
-    }
 
     public void showPanel()
     {
-        panel.SetActive(true);
+        if (panel != null)
+            panel.SetActive(true);
     }
 
     public void hidePanel()
     {
-        panel.SetActive(false);
+        if (panel != null)
+            panel.SetActive(false);
+    }
+
+    public void TogglePanel()
+    {
+        if (panel != null)
+        {
+            Debug.Log($"{name} toggled panel: {panel.name}");
+            panel.SetActive(!panel.activeSelf);
+        }
+        else
+        {
+            Debug.LogWarning($"{name} has no panel assigned.");
+        }
+    }
+
+
+    public GameObject getPart()
+    {
+        return this.gameObject;
     }
 }
